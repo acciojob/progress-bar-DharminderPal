7a0188e603e7ef5d8d7dbb18b5cc2b43c81f0b93
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-31 16:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-//your JS code here. If required.
+        const progress = document.getElementById('progress');
+        const prev = document.getElementById('prev');
+        const next = document.getElementById('next');
+        const circle = document.querySelectorAll('.circle');
 
+        let currentActive = 1;
 
-const progress = document.getElementById('progress');
-const pre = document.getElementById('pre');
-const next = document.getElementById('next');
-const circle = document.querySelectorAll('.circle');
+        next.addEventListener('click', () => {
+            currentActive++;
+            if (currentActive > circle.length) {
+                currentActive = circle.length;
+            }
+            update();
+        });
 
-let currentActive = 1;
+        prev.addEventListener('click', () => {
+            currentActive--;
+            if (currentActive < 1) {
+                currentActive = 1;
+            }
+            update();
+        });
 
-next.addEventListener('click',()=>{
-    currentActive++
-    if(currentActive>circle.length){
-        currentActive = circle.length;
-}
-// console.log(currentActive);
-// console.log(currentActive);
-update();
-});
-pre.addEventListener('click',()=>{
-    currentActive--
-    if(currentActive<1){
-        currentActive = 1;
-}
-// console.log(currentActive);
-// console.log(currentActive);
-update();
-});
+        function update() {
+            circle.forEach((circle, idx) => {
+                if (idx < currentActive) {
+                    circle.classList.add('active');
+                } else {
+                    circle.classList.remove('active');
+                }
+            });
 
+            const actives = document.querySelectorAll('.active');
+            progress.style.width = (actives.length / circle.length) * 100 + '%';
 
-function update(){
-    circle.forEach((circle,idx)=>{
-        if(idx<currentActive){
-           circle.classList.add('active')
-        }else{
-            circle.classList.remove('active')
+            // Toggle button states
+            prev.disabled = currentActive === 1;
+            next.disabled = currentActive === circle.length;
         }
-    })
-    const actives =document.querySelectorAll('.active');
-    progress.style.width=(actives.length/circle.length)*100+'%';
-}
